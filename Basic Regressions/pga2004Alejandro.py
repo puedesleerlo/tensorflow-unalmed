1e8c88a95a86c121ab6e8895058030ea00ce0a57
Alejandro Tabares <atabaresa@unal.edu.co>
2017-02-26 18:59:38 -0500

    inicia regresión

--- Basic Regressions/pga2004Alejandro.py
+++ Basic Regressions/pga2004Alejandro.py
@@ -7,8 +7,47 @@
 
 import tensorflow as tf
 import csv
-
+import numpy
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
 with open('dos_datos.csv') as csvfile:
     reader = csv.DictReader(csvfile)
+    independent = []
+    dependent = []
+    number = 0
     for row in reader:
-        print(row['x'])
+        x = num(row['x'])
+        y = num(row['y'])
+        independent.append(x)
+        dependent.append(y)
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
+
+# Training Data
+train_X = numpy.asarray(independent)
+train_Y = numpy.asarray(dependent)
+n_samples = train_X.shape[0]
+
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Set model weights
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+# Construct a linear model
+pred = tf.add(tf.mul(X, W), b)
+
+# Mean squared error
+cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+# Gradient descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
