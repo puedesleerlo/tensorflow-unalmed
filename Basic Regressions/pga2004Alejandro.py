3ba0b673e0550cb16f5002b6fdb721e0397f8140
Alejandro Tabares <atabaresa@unal.edu.co>
2017-02-26 16:28:08 -0500

    Inicio del repositorio

---
+++ Basic Regressions/pga2004Alejandro.py
@@ -0,0 +1,9 @@
+# Dataset:   pga2004.dat
+
+# Source: sportsillustrated.cnn.com
+
+# Description: Performance statistics and winnings for 196 PGA participants
+# during 2004 season.
+
+import tensorflow as tf
+    
